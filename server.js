670363337b0fd847fdd49e40cbf5a9e59f714a02
lohith <lohith05amal@gmail.com>
2021-10-12 12:39:07 -0400

    MVC

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 
-const routes = require('./routes')
+const routes = require('./controllers')
 
 const sequelize = require('./config/connection');
 
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars');
 
 sequelize.sync({force: false})
 .then(()=>{
-  app.listen(PORT, ()=>console.log('now listening'));
+  app.listen(PORT, ()=>console.log(`now listening at ${PORT}`));
 });
